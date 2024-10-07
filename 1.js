e9bf7417ee6b983b44da4b5e74087b4bf386c50a
voigokas vulgo gustas <159552534+voigok@users.noreply.github.com>
2024-10-07 07:49:59 -0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,18 @@
+function exercicio1(soma){
+    const numeros = [2, 5, 7, 9, 10];
+let soma = 0;
+for (let i = 0; i < numeros.length; i++) {
+  soma += numeros[i];
+}
+console.log("A soma dos elementos é:", soma);
+
+}
+
+function exercicio2(retorna){
+    var meuArray= [0,12,13,2,1273,28,1];
+var maior = Math.max.apply(null, meuArray );
+
+;
+
+
+}
